b2f51eb928a07b606b6c5e5e5afb3b5654215569
Valentin Lorentz <progval+git@progval.net>
2017-01-01 20:18:25 +0100

    Rotates addresses by 64 bits before XORing.

--- src/node.rs
+++ src/node.rs
@@ -11,51 +11,67 @@ pub const PUBLIC_KEY_LENGTH: usize = 32;
 
 pub type Path = [u8; 8];
 
+/// Rotates an IPv6 address 64 bits, which is a required preprocessing
+/// for computing the XOR metric.
+/// See https://github.com/cjdelisle/cjdns/blob/cjdns-v18/doc/Whitepaper.md#the-router
+fn rotate_64(i: &[u8; 16]) -> [u8; 16] {
+    [
+        i[ 8], i[ 9], i[10], i[11], i[12], i[13], i[14], i[15],
+        i[ 0], i[ 1], i[ 2], i[ 3], i[ 4], i[ 5], i[ 6], i[ 7],
+    ]
+}
+
 /// Wrapper of `Ipv6Addr` that implements `GenericId`
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Address {
-    pub octets: [u8; 16],
+    bytes: [u8; 16],
 }
 
 impl Address {
+    pub fn new(bytes: &[u8; 16]) -> Address {
+        Address { bytes: rotate_64(bytes) }
+    }
+    pub fn bytes(&self) -> [u8; 16] {
+        rotate_64(&self.bytes)
+    }
     pub fn from_ipv6addr(ipv6addr: &Ipv6Addr) -> Address {
-        Address { octets: ipv6addr.octets() }
+        Address::new(&ipv6addr.octets())
     }
     pub fn to_ipv6addr(&self) -> Ipv6Addr {
-        Ipv6Addr::from(self.octets)
+        Ipv6Addr::from(self.bytes())
     }
 }
 
 impl GenericId for Address {
     fn bitxor(&self, other: &Self) -> Self {
-        let vec = self.octets.to_vec().bitxor(&other.octets.to_vec());
+        let vec = self.bytes.to_vec().bitxor(&other.bytes.to_vec());
         assert_eq!(vec.len(), 16);
-        let mut octets = [0u8; 16];
-        octets.copy_from_slice(&vec);
-        Address { octets: octets }
+        let mut bytes = [0u8; 16];
+        bytes.copy_from_slice(&vec);
+        Address { bytes: bytes }
     }
     fn is_zero(&self) -> bool {
-        self.octets.to_vec().is_zero()
+        self.bytes.to_vec().is_zero()
     }
     fn bits(&self) -> usize {
-        self.octets.to_vec().bits()
+        self.bytes.to_vec().bits()
     }
     fn gen(bit_size: usize) -> Self {
         let vec = Vec::<u8>::gen(bit_size);
         assert_eq!(vec.len(), 16);
-        let mut octets = [0u8; 16];
-        octets.copy_from_slice(&vec);
-        Address { octets: octets }
+        let mut bytes = [0u8; 16];
+        bytes.copy_from_slice(&vec);
+        Address { bytes: bytes }
     }
     fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
-        self.octets.encode(s)
+        self.bytes.encode(s)
     }
     fn decode<D: Decoder>(d: &mut D) -> Result<Self, D::Error> {
         let vec = try!(Vec::<u8>::decode(d));
         assert_eq!(vec.len(), 16);
-        let mut octets = [0u8; 16];
-        octets.copy_from_slice(&vec);
-        Ok(Address { octets: octets })
+        let mut bytes = [0u8; 16];
+        bytes.copy_from_slice(&vec);
+        Ok(Address { bytes: bytes })
     }
 }
 
