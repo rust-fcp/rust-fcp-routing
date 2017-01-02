8028fa6888eafa7adb6757726e09ffbcba2af8bf
Valentin Lorentz <progval+git@progval.net>
2017-01-02 11:18:34 +0100

    Add NodeStore::get_node.

--- src/node_store.rs
+++ src/node_store.rs
@@ -3,12 +3,26 @@ use dht::GenericNodeTable;
 
 use node::{Address, NodeData, Node};
 
+/// Returns by a request to find a node's path and public key.
+#[derive(Clone, Debug)]
+pub enum GetNodeResult {
+    /// The exact node, if it was found.
+    FoundNode(Node),
+    /// Nodes close to the searched node. They should be queried about
+    /// the searched node.
+    ClosestNodes(Vec<Node>),
+    /// This table knows of no node at all. It should be bootstrapped
+    /// using an external way (ie. find some peers).
+    Nothing,
+}
+
 pub struct NodeStore {
     pub table: KNodeTable<Address, NodeData>,
     pub my_address: Address,
 }
 
 impl NodeStore {
+    /// Creates a new empty NodeStore.
     pub fn new(my_address: Address) -> NodeStore {
         NodeStore {
             table: KNodeTable::new(my_address.clone()),
@@ -16,6 +30,8 @@ impl NodeStore {
         }
     }
 
+    /// Inserts a node in the NodeStore, poping nodes from full
+    /// buckets if necessary.
     pub fn update(&mut self, node: &Node) {
         let insert_suceeded = self.table.update(node);
         if !insert_suceeded {
@@ -24,7 +40,27 @@ impl NodeStore {
         }
     }
 
+    /// Retrurns an ordered vector of nodes, which are the closest to the
+    /// target address this NodeStore knows about.
     pub fn find_closest_nodes(&self, address: &Address, count: usize) -> Vec<Node> {
         self.table.find(address, count)
+    }
+
+    /// Tries to get a node. On failure, returns `nb_closest` nodes (or all
+    /// nodes in the store, if `nb_closest` is too high) that should be
+    /// queried about the searched node.
+    pub fn get_node(&self, address: &Address, nb_closest: usize) -> GetNodeResult {
+        let closest_nodes = self.find_closest_nodes(address, nb_closest);
+        match closest_nodes.clone().get(0) { // TODO: do not clone
+            Some(closest_node) => {
+                if closest_node.id == *address {
+                    GetNodeResult::FoundNode(closest_node.clone())
+                }
+                else {
+                    GetNodeResult::ClosestNodes(closest_nodes)
+                }
+            }
+            None => GetNodeResult::Nothing,
+        }
     }
 }
